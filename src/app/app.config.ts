08cf696336c0ed39f973ac636968205042ed2d42
Khang Tran <khangtrann8198@gmail.com>
2026-05-07 15:32:21 +0700

    feat: add IMAGE_LOADER provider and optimize image handling in event card component

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,6 +5,8 @@ import {
 } from '@angular/core';
 import { provideRouter, withComponentInputBinding, withViewTransitions } from '@angular/router';
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import { IMAGE_LOADER, ImageLoaderConfig } from '@angular/common';
+
 import { routes } from './app.routes';
 import { API_URL } from './core/tokens';
 
@@ -17,5 +19,14 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(withFetch()),
 
     { provide: API_URL, useValue: 'http://localhost:3000' },
+
+    // https://khangtran.dev/cdn-cgi/image/width=500,format=auto/https://storage.googleapis.com/dev-sandbox-09.firebasestorage.app/angular-keynote.png
+    {
+      provide: IMAGE_LOADER,
+      useValue: (config: ImageLoaderConfig) => {
+        const src = config.src.replace('/images/', '');
+        return `https://khangtran.dev/cdn-cgi/image/width=${config.width},format=auto/https://storage.googleapis.com/dev-sandbox-09.firebasestorage.app/${src}`;
+      },
+    },
   ],
 };
